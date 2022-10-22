3f846562ff1f9266ad145ae8b6b85c71b5cd3f7c
Nobel <nobel_lamun@hotmail.com>
2022-10-22 06:36:03 -0230

    fix attempt

--- src/components/First.js
+++ src/components/First.js
@@ -18,35 +18,33 @@ const First = () => {
   return (
     <section className="first-page">
       <div className="project">
-        {" "}
-        <Flip top cascade>
-          <div className="text-info">
-            <h1>Tesla Clone</h1>
-            <div className="technologies-container">
-              <img className="technologies" src={html} alt="html" />
-              <img className="technologies" src={css} alt="css" />
-              <img className="technologies" src={js} alt="js" />
-              <img className="technologies" src={react} alt="react" />
-              <img className="technologies" src={npm} alt="npm" />
-              <img className="technologies" src={git} alt="git" />
-              <img className="technologies" src={prettier} alt="prettier" />
-            </div>
-            <p>
-              Web app designed to look like Tesla's UI. Fully responsive, tablet
-              and phone friendly.
-            </p>
-            <div className="button-container">
-              <a href="https://kobahmi.github.io/tesla-clone/">
-                Live
-                <img src={computer} alt="computer" />
-              </a>
-              <a href="https://github.com/Kobahmi/tesla-clone">
-                Code
-                <img src={github} alt="github" />
-              </a>
-            </div>
+        <div className="text-info">
+          <h1>Tesla Clone</h1>
+          <div className="technologies-container">
+            <img className="technologies" src={html} alt="html" />
+            <img className="technologies" src={css} alt="css" />
+            <img className="technologies" src={js} alt="js" />
+            <img className="technologies" src={react} alt="react" />
+            <img className="technologies" src={npm} alt="npm" />
+            <img className="technologies" src={git} alt="git" />
+            <img className="technologies" src={prettier} alt="prettier" />
           </div>
-        </Flip>
+          <p>
+            Web app designed to look like Tesla's UI. Fully responsive, tablet
+            and phone friendly.
+          </p>
+          <div className="button-container">
+            <a href="https://kobahmi.github.io/tesla-clone/">
+              Live
+              <img src={computer} alt="computer" />
+            </a>
+            <a href="https://github.com/Kobahmi/tesla-clone">
+              Code
+              <img src={github} alt="github" />
+            </a>
+          </div>
+        </div>
+
         <Fade right>
           <img className="info-gif" src={teslagif} alt="tesla gif" />
         </Fade>
@@ -55,36 +53,35 @@ const First = () => {
         <Fade left>
           <img className="info-gif" src={shoppinggif} alt="shoppy" />
         </Fade>
-        <Flip top cascade>
-          <div className="text-info">
-            <h1>Unreal Polish</h1>
-            <div className="technologies-container">
-              <img className="technologies" src={html} alt="html" />
-              <img className="technologies" src={css} alt="css" />
-              <img className="technologies" src={js} alt="js" />
-              <img className="technologies" src={react} alt="react" />
-              <img className="technologies" src={npm} alt="npm" />
-              <img className="technologies" src={git} alt="git" />
-              <img className="technologies" src={prettier} alt="prettier" />
-              <img className="technologies" src={router} alt="router" />
-            </div>
-            <p>
-              Web shopping app to buy nail polish. Fully responsive, tablet and
-              phone friendly.
-            </p>
 
-            <div className="button-container">
-              <a href="https://kobahmi.github.io/shopping-cart/">
-                Live
-                <img src={computer} alt="computer" />
-              </a>
-              <a href="https://github.com/Kobahmi/shopping-cart">
-                Code
-                <img src={github} alt="github" />
-              </a>
-            </div>
+        <div className="text-info">
+          <h1>Unreal Polish</h1>
+          <div className="technologies-container">
+            <img className="technologies" src={html} alt="html" />
+            <img className="technologies" src={css} alt="css" />
+            <img className="technologies" src={js} alt="js" />
+            <img className="technologies" src={react} alt="react" />
+            <img className="technologies" src={npm} alt="npm" />
+            <img className="technologies" src={git} alt="git" />
+            <img className="technologies" src={prettier} alt="prettier" />
+            <img className="technologies" src={router} alt="router" />
           </div>
-        </Flip>
+          <p>
+            Web shopping app to buy nail polish. Fully responsive, tablet and
+            phone friendly.
+          </p>
+
+          <div className="button-container">
+            <a href="https://kobahmi.github.io/shopping-cart/">
+              Live
+              <img src={computer} alt="computer" />
+            </a>
+            <a href="https://github.com/Kobahmi/shopping-cart">
+              Code
+              <img src={github} alt="github" />
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
